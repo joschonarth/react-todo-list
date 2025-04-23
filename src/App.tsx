c90dbfa017697c91eb94514570eecf5a0d838fca
joschonarth <joschonarth@gmail.com>
2025-04-23 19:34:34 -0300

    feat: implement components on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import styles from './App.module.css'
+
+import { PlusCircle } from 'phosphor-react'
+
+import { Button } from './components/Button'
 import { Header } from './components/Header'
+import { Input } from './components/Input'
 
 function App() {
-  return <Header />
+  return (
+    <main>
+      <Header />
+
+      <section className={styles.content}>
+        <div className={styles.taskInfoContainer}>
+          <Input />
+          <Button>
+            Criar
+            <PlusCircle size={16} color="#f2f2f2" weight="bold" />
+          </Button>
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default App
